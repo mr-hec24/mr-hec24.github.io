841eafc3d41ea2afb8735d6ae5d1c7079ef159e9
Hector Armando Rodriguez <33230203+mr-hec24@users.noreply.github.com>
2021-02-23 17:04:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var token = "Bearer "
 var redirect_uri = "https%3A%2F%2Fmr-hec24.github.io%2F";
-var client_id = "";
-var client_secret = "";
+var redirect_uri = "https://makeratplay.github.io/SpotifyWebAPI/"; // change this your value
+//var redirect_uri = "http://127.0.0.1:5500/index.html";
+ 
+
+var client_id = ""; 
+var client_secret = ""; // In a real app you should not expose your client_secret to the user
 
 var access_token = null;
 var refresh_token = null;
@@ -19,28 +23,23 @@ const PREVIOUS = "https://api.spotify.com/v1/me/player/previous";
 const PLAYER = "https://api.spotify.com/v1/me/player";
 const TRACKS = "https://api.spotify.com/v1/playlists/{{PlaylistId}}/tracks";
 const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
-const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle"
+const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
 
-
-
-// let access_token = "BQAy-Ii3isHa8oejnTOf7HNhKLzwX955iigu7hF5_1nQm1gHY1-tAe9b6uTchmducA1XKNE2Y1vsxhaQuBcrXvDgKp_VyRKCTHtkkkLDPoja0CqSEuqntP-qHaRSZcHUhpsF3bT-TPAjW6n_q0pe2j3T2lyb"
-
-function onPageLoad() {
+function onPageLoad(){
     client_id = localStorage.getItem("client_id");
     client_secret = localStorage.getItem("client_secret");
-
-    if ( window.location.search.length > 0) {
+    if ( window.location.search.length > 0 ){
         handleRedirect();
     }
-    else {
+    else{
         access_token = localStorage.getItem("access_token");
-        if (access_token == null) {
-            // we dont have an access token so present token section
-            document.getElementById("tokenSection").style.display = 'block';
+        if ( access_token == null ){
+            // we don't have an access token so present token section
+            document.getElementById("tokenSection").style.display = 'block';  
         }
         else {
             // we have an access token so present device section
-            document.getElemtnById("deviceSection").style.display = 'block';
+            document.getElementById("deviceSection").style.display = 'block';  
             refreshDevices();
             refreshPlaylists();
             currentlyPlaying();
@@ -49,48 +48,47 @@ function onPageLoad() {
     refreshRadioButtons();
 }
 
-function handleRedirect() {
+function handleRedirect(){
     let code = getCode();
-    fetchAccessToken (code);
+    fetchAccessToken( code );
     window.history.pushState("", "", redirect_uri); // remove param from url
 }
 
-function getCode() {
+function getCode(){
     let code = null;
     const queryString = window.location.search;
-    if (queryString.length > 0) {
+    if ( queryString.length > 0 ){
         const urlParams = new URLSearchParams(queryString);
-        code = urlParams.get('code');
+        code = urlParams.get('code')
     }
     return code;
 }
 
-function requestAuthorization() {
+function requestAuthorization(){
     client_id = document.getElementById("clientId").value;
     client_secret = document.getElementById("clientSecret").value;
-
     localStorage.setItem("client_id", client_id);
-    localStorage.setItem("client_secret", client_secret);
+    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
 
     let url = AUTHORIZE;
     url += "?client_id=" + client_id;
     url += "&response_type=code";
-    url += "&redirect_uri=" + redirect_uri;
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
     url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
-    window.location.href = url; // Shows Spotify's Authorization Screen
+    window.location.href = url; // Show Spotify's authorization screen
 }
 
-function fetchAccessToken (code) {
+function fetchAccessToken( code ){
     let body = "grant_type=authorization_code";
-    body += "&code=" + code;
-    body += "&redirect_uri=" + redirect_uri;
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
     body += "&client_id=" + client_id;
     body += "&client_secret=" + client_secret;
     callAuthorizationApi(body);
 }
 
-function refreshAccessToken() {
+function refreshAccessToken(){
     refresh_token = localStorage.getItem("refresh_token");
     let body = "grant_type=refresh_token";
     body += "&refresh_token=" + refresh_token;
@@ -98,7 +96,7 @@ function refreshAccessToken() {
     callAuthorizationApi(body);
 }
 
-function callAuthorizationApi( body ) {
+function callAuthorizationApi(body){
     let xhr = new XMLHttpRequest();
     xhr.open("POST", TOKEN, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -107,16 +105,16 @@ function callAuthorizationApi( body ) {
     xhr.onload = handleAuthorizationResponse;
 }
 
-function handleAuthorizationResponse() {
-    if (this.staust == 200) {
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
         var data = JSON.parse(this.responseText);
         console.log(data);
         var data = JSON.parse(this.responseText);
-        if (data.access_token != undefined) {
+        if ( data.access_token != undefined ){
             access_token = data.access_token;
             localStorage.setItem("access_token", access_token);
         }
-        if (data.refresh_token != undefined) {
+        if ( data.refresh_token  != undefined ){
             refresh_token = data.refresh_token;
             localStorage.setItem("refresh_token", refresh_token);
         }
@@ -128,19 +126,19 @@ function handleAuthorizationResponse() {
     }
 }
 
-function refreshDevices() {
-    callApi( "GET", DEVICES, null, handleDevicesResponse());
+function refreshDevices(){
+    callApi( "GET", DEVICES, null, handleDevicesResponse );
 }
 
-function handleDevicesResponse() {
-    if (this.status == 200) {
+function handleDevicesResponse(){
+    if ( this.status == 200 ){
         var data = JSON.parse(this.responseText);
         console.log(data);
-        removeAllItems("devices");
-        data.devices.forEach (item => addDevice(item));
+        removeAllItems( "devices" );
+        data.devices.forEach(item => addDevice(item));
     }
-    else if (this.status == 401) {
-        refreshAccessToken();
+    else if ( this.status == 401 ){
+        refreshAccessToken()
     }
     else {
         console.log(this.responseText);
@@ -148,17 +146,17 @@ function handleDevicesResponse() {
     }
 }
 
-function addDevice (item) {
+function addDevice(item){
     let node = document.createElement("option");
-    node.vale = item.id;
+    node.value = item.id;
     node.innerHTML = item.name;
-    document.getElementById("playlists").appendChild(node);
+    document.getElementById("devices").appendChild(node); 
 }
 
-function callApi(method, url, body, callback) {
+function callApi(method, url, body, callback){
     let xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
-    xhr.setRequestHeader('Content-Type', "application/json");
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
     xhr.send(body);
     xhr.onload = callback;
@@ -192,7 +190,7 @@ function addPlaylist(item){
     document.getElementById("playlists").appendChild(node); 
 }
 
-function removeAllItems (elementId) {
+function removeAllItems( elementId ){
     let node = document.getElementById(elementId);
     while (node.firstChild) {
         node.removeChild(node.firstChild);
@@ -233,7 +231,14 @@ function previous(){
     callApi( "POST", PREVIOUS + "?device_id=" + deviceId(), null, handleApiResponse );
 }
 
-function handleApiResponse() {
+function transfer(){
+    let body = {};
+    body.device_ids = [];
+    body.device_ids.push(deviceId())
+    callApi( "PUT", PLAYER, JSON.stringify(body), handleApiResponse );
+}
+
+function handleApiResponse(){
     if ( this.status == 200){
         console.log(this.responseText);
         setTimeout(currentlyPlaying, 2000);
