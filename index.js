f9200e2d058b1c06fe63805f2027dafdc8b51c35
mr-hec24 <harmandorod@hotmail.com>
2021-02-25 19:08:50 -0700

    Commented Stuff out

--- index.js
+++ index.js
@@ -416,26 +416,26 @@ function playSpotifyURI() {
         var uri = data.item.uri;
         console.log(uri);
         
-        const play = ({
-            spotify_uri,
-            playerInstance: {
-              _options: {
-                getOAuthToken,
-                id
-              }
-            }
-          }) => {
-            getOAuthToken(access_token => {
-              fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-                method: 'PUT',
-                body: JSON.stringify({ uris: [spotify_uri] }),
-                headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': `Bearer ${access_token}`
-                },
-              });
-            });
-          };
+        // const play = ({
+        //     spotify_uri,
+        //     playerInstance: {
+        //       _options: {
+        //         getOAuthToken,
+        //         id
+        //       }
+        //     }
+        //   }) => {
+        //     getOAuthToken(access_token => {
+        //       fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+        //         method: 'PUT',
+        //         body: JSON.stringify({ uris: [spotify_uri] }),
+        //         headers: {
+        //           'Content-Type': 'application/json',
+        //           'Authorization': `Bearer ${access_token}`
+        //         },
+        //       });
+        //     });
+        //   };
         
         play({
             playerInstance: new Spotify.Player({ name: "GrifRed" }),
