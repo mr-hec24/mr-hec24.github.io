97f3434192882a7d56d315d8a0435337c7e7dbe6
mr-hec24 <harmandorod@hotmail.com>
2021-02-25 18:50:00 -0700

    Play Spotify URI

--- index.js
+++ index.js
@@ -210,20 +210,21 @@ function play(){
     //startTimer(min);
     changeStudyState();
 
+    playSpotifyURI();
 
-    let playlist_id = document.getElementById("playlists").value;
-    let body = {};
-    let album = document.getElementById("album").value;
-    if ( album.length > 0 ){
-        body.context_uri = album;
-    }
-    else{
-        body.context_uri = "spotify:playlist:" + playlist_id;
-    }
-    body.offset = {};
-    body.offset.position = 0;
-    body.offset.position_ms = 0;
-    callApi( "PUT", PLAY + "?device_id=" + deviceId(), JSON.stringify(body), handleApiResponse );
+    // let playlist_id = document.getElementById("playlists").value;
+    // let body = {};
+    // let album = document.getElementById("album").value;
+    // if ( album.length > 0 ){
+    //     body.context_uri = album;
+    // }
+    // else{
+    //     body.context_uri = "spotify:playlist:" + playlist_id;
+    // }
+    // body.offset = {};
+    // body.offset.position = 0;
+    // body.offset.position_ms = 0;
+    // callApi( "PUT", PLAY + "?device_id=" + deviceId(), JSON.stringify(body), handleApiResponse );
 }
 
 function changeStudyState() {
@@ -407,7 +408,7 @@ function readySDK() {
     };
 }
 
-function playSpotifyURI(spotify_uri, getOAuthToken, id) {
+function playSpotifyURI() {
     if (this.status == 200) {
         console.log("Playing Spotify URI Function:")
         var data = JSON.parse(this.responseText);
